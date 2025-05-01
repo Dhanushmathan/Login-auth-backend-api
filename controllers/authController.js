863af060407a6b023dc18cab005a50ef49ffdcbb
Dhanushmathan <lovelymathan2394@gmail.com>
2025-05-01 15:53:10 +0530

    fix: module error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcryptjs';
-import UserModel from '../models/User';
+import UserModel from '../models/User.js';
 import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
