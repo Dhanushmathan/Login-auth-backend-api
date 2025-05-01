b14d0790b55740e06f854465cbecd6d67a86b5fc
Dhanushmathan <lovelymathan2394@gmail.com>
2025-05-01 15:24:19 +0530

    register and login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,42 @@
 import bcrypt from 'bcryptjs';
+import UserModel from '../models/User';
+import jwt from 'jsonwebtoken';
 
 const register = async (req, res) => {
     const { username, email, password } = req.body;
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        
+        const newUser = await UserModel.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+        res.status(201).json({ message: "User registeed", user: newUser });
     } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 
+}
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ message: "Login successful", token });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 
 }
