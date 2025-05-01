68fb3e06b5184b06fa6f9a9042f142d59ba58337
Dhanushmathan <lovelymathan2394@gmail.com>
2025-05-01 15:30:33 +0530

    login routes functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcryptjs';
 import UserModel from '../models/User';
 import jwt from 'jsonwebtoken';
 
-const register = async (req, res) => {
+export const register = async (req, res) => {
     const { username, email, password } = req.body;
 
     try {
@@ -19,7 +19,7 @@ const register = async (req, res) => {
 
 }
 
-const login = async (req, res) => {
+export const login = async (req, res) => {
     const { email, password } = req.body;
 
     try {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,16 @@
 // login routes
 import express from "express";
 import LoginModel from "../models/User.js";
+import { login, register } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json({ message: "Login API" });
+router.post("/login", login);
+router.post("/register", register);
+
+// Protecting routes
+router.get("/protected", auth, (req, res) => {
+    res.json({ message: `Welcome user ${req.user.id}`});
 })
-
-
 
 export default router;
